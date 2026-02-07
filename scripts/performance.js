91dd97c60dd0f7a3f83778821e7ebc93c0d0e0c9
Henry <183124787+HWaterCoding@users.noreply.github.com>
2026-02-07 02:31:49 -0500

    Fix the lapTimers <div> positioning Make lapTimers a scrollable box to hold the laps

--- scripts/performance.js
+++ scripts/performance.js
@@ -92,17 +92,17 @@ function lapTime(stopwatch){
             return;
         }
 
-        lapTimers.style.display = "block";
+        lapTimers.style.display = "flex";
 
         const elapsedMs = stopwatch.getElapsedMs();
         const minutes = Math.floor(elapsedMs / 60000);
         const seconds = Math.floor(elapsedMs / 1000) % 60;
         const centiseconds = Math.floor((elapsedMs % 1000) / 10);
         
-        const lapH3 = document.createElement("h3");
-        lapH3.innerHTML = `Lap ${lapCount}: ${pad2(minutes)}:${pad2(seconds)}:${pad2(centiseconds)}`;
-        lapH3.classList.add("lapH3");
-        lapTimers.appendChild(lapH3);
+        const lap = document.createElement("p");
+        lap.innerHTML = `Lap ${lapCount}: ${pad2(minutes)}:${pad2(seconds)}:${pad2(centiseconds)}`;
+        lap.classList.add("lap");
+        lapTimers.appendChild(lap);
 
         lapCount++;
     }
