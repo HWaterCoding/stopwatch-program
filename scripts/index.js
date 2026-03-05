45e9792046114750a73f732cfeba8b93866d6f4c
Henry <183124787+HWaterCoding@users.noreply.github.com>
2026-03-04 23:32:04 -0500

    Add buttons and HTML layout skeleton to append to Create new JS modules Cache buttons and add eventlistener layout

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+//main file, add event listeners.
+
+const clockBtn = document.getElementById("clockBtn");
+clockBtn.addEventListener("click", () =>{
+
+});
+
+const stopwatchBtn = document.getElementById("stopwatchBtn");
+stopwatchBtn.addEventListener("click", () =>{
+
+});
+
+const timerBtn = document.getElementById("timerBtn");
+timerBtn.addEventListener("click", () =>{
+
+});
+
+const alarmBtn = document.getElementById("alarmBtn");
+alarmBtn.addEventListener("click", () =>{
+
+});
