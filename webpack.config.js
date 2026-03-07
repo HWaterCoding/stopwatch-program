19b759c0081ebcd3a8d90a630f055e982d3f2018
Henry <183124787+HWaterCoding@users.noreply.github.com>
2026-03-07 02:29:19 -0500

    More filepath changes and config entry point update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 export default {
   mode: "development",
 
-  entry: "./src/scripts/index.js",
+  entry: "./src/apps/index.js",
 
   output: {
     filename: "main.js",
